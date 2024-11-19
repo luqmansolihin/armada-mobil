57bb93d9ac874b1d6a3294be415b6b5145c626d5
luqmansolihin <officialluqmansolihin@gmail.com>
2024-11-19 15:28:50 +0700

    Creata a View of Form to store Outlet

--- resources/views/pages/cms/outlets/create.blade.php
+++ resources/views/pages/cms/outlets/create.blade.php
@@ -22,8 +22,8 @@
                             <label for="name" class="col-sm-2 col-form-label">Name</label>
                             <div class="col-sm-6">
                                 <input type="text" id="name" name="name"
-                                       class="form-control @error('name') is-invalid @enderror"
-                                       value="{{ old('name') }}" required />
+                                    class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}"
+                                    required />
                                 @error('name')
                                     <span class="error invalid-feedback">{{ $message }}</span>
                                 @enderror
@@ -33,7 +33,7 @@
                             <label for="address" class="col-sm-2 col-form-label">Address</label>
                             <div class="col-sm-6">
                                 <textarea name="address" class="form-control @error('address') is-invalid @enderror" id="address"
-                                          placeholder="Address" rows="2" required>{{ old('address') }}</textarea>
+                                    placeholder="Address" rows="2" required>{{ old('address') }}</textarea>
                                 @error('address')
                                     <span class="error invalid-feedback">{{ $message }}</span>
                                 @enderror
@@ -43,8 +43,8 @@
                             <label for="phone" class="col-sm-2 col-form-label">Phone</label>
                             <div class="col-sm-6">
                                 <input type="text" id="phone" name="phone"
-                                       class="form-control @error('phone') is-invalid @enderror"
-                                       value="{{ old('phone') }}" required />
+                                    class="form-control @error('phone') is-invalid @enderror" value="{{ old('phone') }}"
+                                    required />
                                 @error('phone')
                                     <span class="error invalid-feedback">{{ $message }}</span>
                                 @enderror
@@ -54,8 +54,7 @@
                             <label for="fax" class="col-sm-2 col-form-label">Fax</label>
                             <div class="col-sm-6">
                                 <input type="text" id="fax" name="fax"
-                                       class="form-control @error('fax') is-invalid @enderror"
-                                       value="{{ old('fax') }}"/>
+                                    class="form-control @error('fax') is-invalid @enderror" value="{{ old('fax') }}" />
                                 @error('fax')
                                     <span class="error invalid-feedback">{{ $message }}</span>
                                 @enderror
@@ -65,8 +64,8 @@
                             <label for="email" class="col-sm-2 col-form-label">Email</label>
                             <div class="col-sm-6">
                                 <input type="text" id="email" name="email"
-                                       class="form-control @error('email') is-invalid @enderror"
-                                       value="{{ old('email') }}" required />
+                                    class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}"
+                                    required />
                                 @error('email')
                                     <span class="error invalid-feedback">{{ $message }}</span>
                                 @enderror
@@ -76,8 +75,8 @@
                             <label for="url_address" class="col-sm-2 col-form-label">URL Address</label>
                             <div class="col-sm-6">
                                 <input type="text" id="url_address" name="url_address"
-                                       class="form-control @error('url_address') is-invalid @enderror"
-                                       value="{{ old('url_address') }}" required />
+                                    class="form-control @error('url_address') is-invalid @enderror"
+                                    value="{{ old('url_address') }}" required />
                                 @error('url_address')
                                     <span class="error invalid-feedback">{{ $message }}</span>
                                 @enderror
@@ -86,24 +85,24 @@
                         <div class="form-group row">
                             <label for="url_embed_address" class="col-sm-2 col-form-label">URL Embed Address</label>
                             <div class="col-sm-6">
-                                <textarea name="url_embed_address" class="form-control @error('url_embed_address') is-invalid @enderror" id="url_embed_address"
-                                          placeholder="URL Embed Address" rows="2" required>{{ old('url_embed_address') }}</textarea>
+                                <textarea name="url_embed_address" class="form-control @error('url_embed_address') is-invalid @enderror"
+                                    id="url_embed_address" placeholder="URL Embed Address" rows="2" required>{{ old('url_embed_address') }}</textarea>
                                 @error('url_embed_address')
-                                `<span class="error invalid-feedback">{{ $message }}</span>
+                                    `<span class="error invalid-feedback">{{ $message }}</span>
                                 @enderror
                             </div>
                         </div>
                         <fieldset class="form-group row">
                             <legend class="col-form-label col-sm-2 float-sm-left pt-0 font-weight-bold">Service</legend>
                             <div class="col-sm-10">
-                                @foreach($services as $service)
+                                @foreach ($services as $service)
                                     <div class="form-check">
-                                        <input class="form-check-input @error('outlets.[$loop->index][service]') is-invalid @enderror"
-                                               name="outlets[{{$loop->index}}][service]"
-                                               type="checkbox" id="{{ 'checkBox'.$loop->index }}"
-                                               value="{{ $service->id }}"
+                                        <input
+                                            class="form-check-input @error('outlets.[$loop->index][service]') is-invalid @enderror"
+                                            name="outlets[{{ $loop->index }}][service]" type="checkbox"
+                                            id="{{ 'checkBox' . $loop->index }}" value="{{ $service->id }}"
                                             @checked(old('outlets.[$loop->index].[service]') == $service->id)>
-                                        <label class="form-check-label" for="{{ 'checkBox'.$loop->index }}">
+                                        <label class="form-check-label" for="{{ 'checkBox' . $loop->index }}">
                                             {{ $service->title }}
                                         </label>
                                         @error('outlets.[$loop->index][service]')
@@ -113,60 +112,72 @@
                                 @endforeach
                             </div>
                         </fieldset>
+
                         <div class="form-group row">
                             <label for="day_from0" class="col-sm-2 col-form-label">Day from</label>
                             <div class="col-sm-4">
                                 <select name="outlets[0][day_from]"
-                                    class="form-control custom_select @error('day_from') is-invalid @enderror"
+                                    class="form-control custom_select @error('outlets[0][day_from]') is-invalid @enderror"
                                     id="day_from0" placeholder="Choose Day" required>
                                     @foreach (App\Enums\DayEnum::cases() as $day)
-                                        <option value="{{ $day->value }}" @selected($day->value == old('day_from'))>
+                                        <option value="{{ $day->value }}" @selected($day->value == old('outlets[0][day_from]'))>
                                             {{ strtoupper($day->value) }}
                                         </option>
                                     @endforeach
                                 </select>
-                                @error('day_from')
+                                @error('outlets[0][day_from]')
                                     <span class="error invalid-feedback">{{ $message }}</span>
                                 @enderror
                             </div>
 
-                            <label for="day_from" class="col-sm-2 col-form-label">Day to</label>
+                            <label for="day_to0" class="col-sm-2 col-form-label">Day to</label>
                             <div class="col-sm-4">
-                                <select name="day_to"
-                                    class="form-control custom_select @error('day_to') is-invalid @enderror" id="day_to"
-                                    placeholder="Choose Day" required>
+                                <select name="outlets[0][day_to]"
+                                    class="form-control custom_select @error('outlets[0][day_to]') is-invalid @enderror"
+                                    id="day_to0" placeholder="Choose Day" required>
                                     @foreach (App\Enums\DayEnum::cases() as $day)
-                                        <option value="{{ $day->value }}" @selected($day->value == old('day_to'))>
+                                        <option value="{{ $day->value }}" @selected($day->value == old('outlets[0][day_to]'))>
                                             {{ strtoupper($day->value) }}
                                         </option>
                                     @endforeach
                                 </select>
-                                @error('day_to')
+                                @error('outlets[0][day_to]')
                                     <span class="error invalid-feedback">{{ $message }}</span>
                                 @enderror
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="open_time" class="col-sm-2 col-form-label">Open Time</label>
+                            <label for="open_time0" class="col-sm-2 col-form-label">Open Time</label>
                             <div class="col-sm-4">
-                                <input type="text" id="open_time" name="open_time"
-                                    class="form-control datetimepicker-input @error('open_time') is-invalid @enderror"
-                                    value="{{ old('open_time') }}" data-target="#open_time" data-toggle="datetimepicker"
-                                    autocomplete="off" />
-                                @error('open_time')
+                                <input type="text" id="open_time0" name="outlets[0][open_time]"
+                                    class="form-control datetimepicker-input @error('outlets[0][open_time]') is-invalid @enderror"
+                                    value="{{ old('outlets[0][open_time]') }}" data-target="#open_time0"
+                                    data-toggle="datetimepicker" autocomplete="off" />
+                                @error('outlets[0][open_time]')
                                     <span class="error invalid-feedback">{{ $message }}</span>
                                 @enderror
                             </div>
 
-                            <label for="close_time" class="col-sm-2 col-form-label">Close Time</label>
+                            <label for="close_time0" class="col-sm-2 col-form-label">Close Time</label>
                             <div class="col-sm-4">
-                                <input type="text" id="close_time" name="close_time"
-                                    class="form-control datetimepicker-input @error('close_time') is-invalid @enderror"
-                                    value="{{ old('close_time') }}" data-target="#close_time" data-toggle="datetimepicker"
-                                    autocomplete="off" />
-                                @error('close_time')
+                                <input type="text" id="close_time0" name="outlets[0][close_time]"
+                                    class="form-control datetimepicker-input @error('outlets[0][close_time]') is-invalid @enderror"
+                                    value="{{ old('outlets[0][close_time]') }}" data-target="#close_time0"
+                                    data-toggle="datetimepicker" autocomplete="off" />
+                                @error('outlets[0][close_time]')
                                     <span class="error invalid-feedback">{{ $message }}</span>
                                 @enderror
+                            </div>
+
+                            <div class="col-sm-2 mt-3">
+                                <button type="button" class="btn btn-danger btn-remove">Remove</button>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-2">
+                                <button type="button" class="btn btn-success" id="add-operational-hour">Add Operational
+                                    Hour</button>
                             </div>
                         </div>
                     </div>
@@ -188,12 +199,12 @@
     </script>
     <script>
         $(function() {
-            $('#open_time').datetimepicker({
+            $('#open_time0').datetimepicker({
                 format: 'HH:mm',
                 stepping: 15
             })
 
-            $('#close_time').datetimepicker({
+            $('#close_time0').datetimepicker({
                 format: 'HH:mm',
                 stepping: 15
             })
